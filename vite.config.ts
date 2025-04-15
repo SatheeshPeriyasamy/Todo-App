9c85be1819ef2732726698939cd936729809765f
Satheesh Periyasamy <satheesh.p@jmangroup.com>
2025-04-15 17:30:45 +0530

    url change

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()]
-// });
-
-// filepath: c:\Users\SatheeshPeriyasamy\Desktop\Todo-App\vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://todobackendexpressapi-env.eba-ess5haam.ap-south-1.elasticbeanstalk.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [react()]
 });
